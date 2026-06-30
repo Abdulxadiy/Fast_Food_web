9f33187a29fead8ed724403c905fce0d2c915f32
Abdulxadiy Abduraximov <abdulhadiabdurahimov4@gmail.com>
2026-06-30 05:42:05 +0500

    Remove WhiteNoise middleware and static storage
    Removed WhiteNoise middleware and storage configuration.

--- maxway/settings.py
+++ maxway/settings.py
@@ -53,7 +53,6 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
-    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -134,7 +133,6 @@ USE_TZ = True
 STATIC_URL = "/static/"
 STATIC_ROOT = BASE_DIR / 'staticfiles'
 STATICFILES_DIRS = [BASE_DIR.joinpath('static')]
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedStaticFilesStorage'
 
 
 MEDIA_ROOT = BASE_DIR.joinpath('media')
